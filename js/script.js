02d618abd87656a4830c2325bdceac18a022c314
Assar <assarlannerbornzz@gmail.com>
2023-05-18 17:41:19 +0200

    It has been working for a while and it is better now, but it is not perfect-mI have a problem with supposed simple answers getting 20 decimals with some rounding error at the end. for example, 0.2 + 0.1 = 0.30000000000000000000000002. This is a known problem in all of programing and it is very annoying in this case.

--- js/script.js
+++ js/script.js
@@ -1,171 +1,214 @@
 
-let buttons = document.getElementsByTagName("button");
+const buttons = document.getElementsByTagName("button");
 
+class ArrayItem {
 
-window.onload = function(){
+    constructor(type, value, hasComma = false,) {
+        this.type = type
+        this.value = value
+        this.hasComma = hasComma
+    }
+}
 
-    console.log(buttons);
+window.onload = function () {
+
     for (let index = 0; index < buttons.length; index++) {
-        
-        buttons[index].addEventListener("click", (event)=>{
 
+        buttons[index].addEventListener("click", (event) => {
 
             btnClick(event);
-            
-            
+
         })
-        
+
     }
 }
 
 let lastClickedWasAnOperation = false;
 let calcArray = [];
-let currNumber = "";
-function btnClick(event){
+let currNumberItem = new ArrayItem("Number", 0)
 
-    clickedBTN = event.target; 
+function btnClick(event) {
 
+    clickedBTN = event.target;
 
+    if (clickedBTN.id === "enter") {
 
-    if (clickedBTN.id === "enter"){
-
-        console.log(calcArray);
-
-                // do complicated calculation
-        if (lastClickedWasAnOperation){
+        if (lastClickedWasAnOperation) {
             calcArray.pop();
-            console.log("herererererer");
+            // dont need to push currNumber since that will have already been done when the now deleted operation was first added.
         }
-        else{
-            calcArray.push(currNumber);
-            
+        else {
+            calcArray.push(currNumberItem);
+            // i Have to add the current number the the calcArray since it hasn't been added yet.
         }
 
-        console.log(calcArray);
-        const answer = theCalculation(calcArray);
+        const answer = theCalculation(calcArray); // this produces the answer to display. It takes an array of alternating numbers and operations.
+        let temp = answer.toString();
+        let comma = false;
+        for (let index = 0; index < temp.length; index++) {
+            if (temp[index] == ".") {
+                comma = true; // The previous information for each number Arrayitem (if they had comma or not) was lost, so i have to manually check. 
+            }
 
+        }
         document.getElementById("lcd").value = answer;
-        
-        currNumber = answer;
+        currNumberItem = new ArrayItem("Number", answer, comma, howManyDecimalsDoesNumberHave(answer)); // by putting the calculated answer as the currant item and emptying the calcArray, I can use the calculated answer in new calcualtion.
         calcArray = [];
         lastClickedWasAnOperation = false;
 
 
     }
-    else if (clickedBTN.id === "clear"){
+    else if (clickedBTN.id === "clear") { // Very easy to implement, All memory variables are reset as well as the displayed calcualtion.
 
-        currNumber = "";
+        currNumberItem = new ArrayItem("Number", 0); 
         calcArray = [];
         document.getElementById("lcd").value = "";
     }
 
-    else{
-        if (charisOperation(clickedBTN.id)){
+    else if (charisOperation(clickedBTN.id)) {
 
-            console.log("operation")
-            if (lastClickedWasAnOperation == false){
-                lastClickedWasAnOperation = true;
-                calcArray.push(currNumber);
-                currNumber = "";
-                calcArray.push(clickedBTN.innerHTML);
-                document.getElementById("lcd").value += clickedBTN.innerHTML;
-            }
-
-        }
-        else if (clickedBTN.id == "comma"){
-            console.log("comma!");
-            currNumber += ".";
-            document.getElementById("lcd").value += clickedBTN.innerHTML;
-        }
-        else{
-            console.log("number")
-
-            lastClickedWasAnOperation = false;
-            currNumber += clickedBTN.innerHTML;
+        if (lastClickedWasAnOperation == false) { // lastClickedWasAnOperation variable prevents user from clicking on an operation twice in a row, as that would break the program.
+            lastClickedWasAnOperation = true;
+            calcArray.push(currNumberItem);
+            currNumberItem = new ArrayItem("Number", 0);
+            calcArray.push(new ArrayItem("Operation", clickedBTN.innerHTML));
             document.getElementById("lcd").value += clickedBTN.innerHTML;
         }
 
     }
 
+    else if (clickedBTN.id == "comma") {
+        if (!currNumberItem.hasComma) { // I implemented the ArratItem class solely because i wanted to be able to keep track if each item had comma or not. This is important because the comma shouldn't be used more than once for each number.
+            currNumberItem.hasComma = true;
+            document.getElementById("lcd").value += ".";
+        }
 
-} 
+    }
 
-function theCalculation(theCalc){
+    else {
+        console.log("number")
+        lastClickedWasAnOperation = false;
 
+        // here is code for a feture that could easily be done by converting number to string; however i felt that was cheating.
+        // When clicking on a number the currant number should get larger. The system for this changes depending on if the currant number has a comma or not.
+        if (currNumberItem.hasComma) {
+            console.log("currnumber: ", currNumberItem.value)
+
+            if (currNumberItem.value >= 0){
+                currNumberItem.value += Number(clickedBTN.innerHTML) * Math.pow(10, (-howManyDecimalsDoesNumberHave(currNumberItem.value)) - 1) 
+
+            }
+            else{ // This needs to be done when i add to a negative number instead.
+                currNumberItem.value -= Number(clickedBTN.innerHTML) * Math.pow(10, (-howManyDecimalsDoesNumberHave(currNumberItem.value)) - 1) 
+
+            }
+
+
+        }
+        else {
+            currNumberItem.value = (currNumberItem.value * 10) + Number(clickedBTN.innerHTML);
+        }
+        document.getElementById("lcd").value += clickedBTN.innerHTML;
+
+    }
+
+}
+
+
+function theCalculation(theCalc) { // This function takes array of numbers and operations and returns one number.
+
+    
+    // According to precedence rules, multiplication and division come before addtion and subtraction; therefore, i do these in two separate steps.
     let onlyPlusAndMinusLeft = doDivAndMul(theCalc);
-    console.log("Multiplication and Division is done! " ,onlyPlusAndMinusLeft);
     let answer = subAndAdd(onlyPlusAndMinusLeft);
 
-    console.log("Addition and subtraction is done!", answer);
     return answer;
 
 }
 
-function doDivAndMul(array){
-// 5+3*2+5/2
+function doDivAndMul(array) {
     for (let index = 0; index < array.length; index++) {
-        
-        
-        if (array[index] == "x"){
-            
-            let before = array.slice(0, index-1);
-            let after = array.slice(index+2);
 
-            newValue = (Number(array[index-1])*Number(array[index+1])).toString(); // i could implement this myself but this is a very common operation.
 
-            before.push(newValue);
-            array = before.concat(after);
-            
-            index -= 2;
+        if (array[index].value == "x") { // I look for Arraitems with value of "x". if i find one i know the the item before and after should be multiplied. "x" will never be found first or last, so an exception will not be thrown.
 
-        }
-        else if (array[index] == "/"){
+            let before = array.slice(0, index - 1); // the arry before and after the two numbers should be joined with the new product in the middle.
+            let after = array.slice(index + 2);
 
-            let before = array.slice(0, index-1);
-            let after = array.slice(index+2);
-
-            newValue = (Number(array[index-1])/Number(array[index+1])).toString();
+            newValue = new ArrayItem("Number", (array[index - 1].value * array[index + 1].value));
 
             before.push(newValue);
             array = before.concat(after);
-            
+
+            index -= 2; // since 2 numbers and 1 operation has produced 1 number(3 turned into 1), index needs to be decreased by 2.
+
+        }
+        else if (array[index].value == "/") { // the exact same except for changing "x" to "/"
+
+            console.log("test!")
+            let before = array.slice(0, index - 1);
+            let after = array.slice(index + 2);
+
+            newValue = new ArrayItem("Number", (array[index - 1].value / array[index + 1].value));
+
+            before.push(newValue);
+            array = before.concat(after);
+
             index -= 2;
 
         }
-        
-        
+
+
     }
     return array;
 }
 
-function subAndAdd(array){
+function subAndAdd(array) { // This last step is very easy. I no longer need to worry about priority so i can just add or subtract terms to the toReturn variable.
+    
+    let toReturn = 0;
+    let Comma = false;
 
-    toReturn = 0;
-
-    toReturn += Number(array[0]);
+    toReturn += array[0].value;
     for (let index = 1; index < array.length; index++) {
 
-        if (array[index] == "+"){
-            toReturn += Number(array[index+1])
+
+        if (array[index].value == "+") {
+            toReturn += array[index + 1].value
         }
-        else if (array[index] == "-")
-        {
-            toReturn -= Number(array[index+1])
+        else if (array[index].value == "-") {
+            toReturn -= array[index + 1].value
         }
     }
 
-    return toReturn.toString();
+    return toReturn
 }
 
-function charisOperation(char){
+function charisOperation(char) { // Checks the clicked button's id for if it is operation (returns true if an operation has been pressed)
 
+    
     let returnValue = false;
-     if (char == "add" || char == "sub" || char == "div"|| char == "mul"){
+    if (char == "add" || char == "sub" || char == "div" || char == "mul") {
 
         returnValue = true;
-     }
+    }
 
-     return returnValue;
+    return returnValue;
+
+}
+
+function howManyDecimalsDoesNumberHave(number) { // returns the number of deciamls a number has. It is useful when i have to add to a number which has a comma.
+
+    let decimals = 0
+    if (number != Math.floor(number)){
+
+        decimals = (number.toString()).split('.')[1].length 
+    }
+    else{
+        decimals = 0
+    }
+
+    console.log("decimals of currnumber; ", decimals)
+    return decimals;
 
 }
 
